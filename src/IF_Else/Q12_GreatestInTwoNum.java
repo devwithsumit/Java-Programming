e9bbdc674e01d53ec087f670c135ce4f6fbe119e
webwithsumit <sumit2410kushwaha@gmail.com>
2024-10-22 19:48:25 +0530

    greatest from three numbers

---
+++ src/IF_Else/Q12_GreatestInTwoNum.java
@@ -0,0 +1,15 @@
+package IF_Else;
+
+public class Q12_GreatestInTwoNum {
+    public static void main(String[] args) {
+        int a = 22;
+        double b = 22.999f;
+        if(a == b){
+            System.out.println("equal");
+        } else if (a > b) {
+            System.out.println("a: " + a);
+        }else{
+            System.out.println("b: "+ b);
+        }
+    }
+}
