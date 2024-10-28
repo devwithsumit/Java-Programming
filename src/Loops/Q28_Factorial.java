88c9903b1c58ccf66aed9d74b0088870c41af906
webwithsumit <sumit2410kushwaha@gmail.com>
2024-10-28 23:02:32 +0530

    Loops - factorial +

---
+++ src/Loops/Q28_Factorial.java
@@ -0,0 +1,15 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Q28_Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        int fact = 1;
+        for (int i = n; i >= 1; i--) {
+            fact *= i;
+            System.out.print(i + (i == 1 ? "" : " * "));
+        }
+        System.out.printf(" = %d", fact);
+    }
+}
