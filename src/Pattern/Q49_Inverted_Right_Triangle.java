048106f3b966358d316d4e74ea22a07cd628449f
webwithsumit <sumit2410kushwaha@gmail.com>
2024-11-05 20:01:11 +0530

    Pattern - inverted right triangle +

---
+++ src/Pattern/Q49_Inverted_Right_Triangle.java
@@ -0,0 +1,18 @@
+package Pattern;
+
+public class Q49_Inverted_Right_Triangle {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+//            // Alternate Method;
+//            for (int j = 1; j <= n; j++) {
+//                if (i + j <= n + 1) System.out.print("* ");
+//                else System.out.print("- ");
+//            }
+            System.out.println();
+        }
+    }
+}
