ce7ce9538f4b6f07444efc51b8082fcdd0083c8b
webwithsumit <sumit2410kushwaha@gmail.com>
2024-11-18 22:25:41 +0530

    Practice - max count deleting right side of the max until max = arr[0] +

--- src/Practice/Q18_MaxElementDeletionCount.java
+++ src/Practice/Q18_MaxElementDeletionCount.java
@@ -1,15 +1,13 @@
 package Practice;
 
-import java.util.Arrays;
-
-public class Q18_MaxElementDeletion {
+public class Q18_MaxElementDeletionCount {
     public static void main(String[] args) {
         int[] arr = {2, 3, 5, 4, 1};
         int size = arr.length, count = 0;
         while (size > 0) {
             int maxIndex = 0;
             for (int i = 0; i < size; i++) {
-                if (arr[i] > arr[maxIndex]) {
+                if (arr[i] > arr[maxIndex]){
                     maxIndex = i;
                 }
             }
