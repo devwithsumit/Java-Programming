4a26a3fd4065a25344c9bd36c83717095829e45f
webwithsumit <sumit2410kushwaha@gmail.com>
2024-11-21 19:07:42 +0530

    Arrays - bubble sort +

---
+++ src/Arrays/Q66_BubbleSort.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import javax.swing.*;
+import java.util.Arrays;
+
+public class Q66_BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 7, 2, 8, 9, 0, 1};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
