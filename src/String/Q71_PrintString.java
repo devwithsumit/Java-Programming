50f879a7dc70df5dc55e7b6f5e8c27f1ef05b075
webwithsumit <sumit2410kushwaha@gmail.com>
2024-11-24 20:58:32 +0530

    Arrays - print string +

---
+++ src/String/Q71_PrintString.java
@@ -0,0 +1,10 @@
+package String;
+
+public class Q71_PrintString {
+    public static void main(String[] args) {
+        String s = "hello";
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+    }
+}
