35b2eb45f344c140abd84cb8957351d84a0bc14a
webwithsumit <sumit2410kushwaha@gmail.com>
2024-11-25 20:27:17 +0530

    Object-oriented programming introduction +

---
+++ src/OOPS/IntroToOops.java
@@ -0,0 +1,19 @@
+package OOPS;
+
+public class IntroToOops {
+    public static void main(String[] args) {
+
+//       1. init via constructor
+        Student obj1  = new Student();
+
+//       2. initialization via reference
+//        obj1.name = "Sumit";
+//        obj1.age = 20;
+
+//       3. init via method
+//        obj1.init("Sumit", 21);
+
+
+        System.out.println(obj1);
+    }
+}
