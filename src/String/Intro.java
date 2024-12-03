b723ac1eda756b7bad713ad1e3948deecd5ecbc2
webwithsumit <sumit2410kushwaha@gmail.com>
2024-12-03 21:56:51 +0530

    String - intro part +

---
+++ src/String/Intro.java
@@ -0,0 +1,26 @@
+package String;
+
+public class Intro {
+    public static void main(String[] args) {
+
+        //methods for initialising a String
+        /*1*/String a = new String("method1");
+        /*2*/String b = new String(new char[]{'m', 't', 'd'});
+        /*3*/String c = "Hello";
+
+        //compareTo && compareToIgnoreCase
+        String str1 = "a";
+        Character ch = 'a';
+//        System.out.println(ch == "a");
+        String str2 = "ABCDFG";
+        //compareToIgnoreCase returns only the size difference
+        System.out.println(str1.compareToIgnoreCase(str2));
+        //compareTo returns the size difference as well as ASCII value difference
+        // the UpperCase and the lowercase character are treated different;
+        System.out.println(str1.compareTo(str2));
+
+        //equals method to compare string ( str1 == str2 is not Allowed)
+        System.out.println(str1.equals(str2));
+
+    }
+}
