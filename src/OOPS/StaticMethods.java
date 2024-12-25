0a9b4f4df97d8fbf5bcb68341c500336bb76152c
webwithsumit <sumit2410kushwaha@gmail.com>
2024-12-25 16:05:41 +0530

    OOPS - static methods

---
+++ src/OOPS/StaticMethods.java
@@ -0,0 +1,19 @@
+package OOPS;
+// Definition: Methods declared with the static keyword belong to the class, not a specific object.
+public class StaticMethods {
+    /*Key Features:
+      • Can be invoked using the class name.
+      • Can access only static fields or methods directly.
+      • Cannot access instance fields or methods unless explicitly passed an object reference.*/
+    static int a;
+    static void method1(){
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        StaticMethods obj = new StaticMethods();
+        /* If tried to invoke via object's reference then
+        reference name will be replaced by class name at runtime.*/
+        obj.method1();
+    }
+}
